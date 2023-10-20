245d91e43e2c05f95605134fa69cc4ba9843a984
Razenth <santiago.gonzalezagudelo@gmail.com>
2023-10-20 07:20:10 -0500

    feat: :sparkles: Archivos Contexto, ApplicationServices y haciendo inyeccion en dependencias

---
+++ RopaApi/Extensions/ApplicationServicesExtensions.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCoreRateLimit;
+
+namespace RopaApi.Extensions
+{
+    public static class ApplicationServicesExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            });
+        });
+        
+        
+        public static void ConfigureRateLimiting(this IServiceCollection services)
+            {
+                services.AddMemoryCache();
+                services.AddSingleton<IRateLimitConfiguration, RateLimitConfiguration>();
+                services.AddInMemoryRateLimiting();
+        
+                services.Configure<IpRateLimitOptions>(options =>
+                {
+                    options.EnableEndpointRateLimiting = true;
+                    options.StackBlockedRequests = false;
+                    options.HttpStatusCode = 429;
+                    options.RealIpHeader = "X-Real-IP";
+                    options.ClientWhitelist = new List<string>();
+        
+                    options.GeneralRules = new List<RateLimitRule>
+                    {
+                        new RateLimitRule
+                        {
+                            Endpoint = "*",
+                            Period = "10s",
+                            Limit = 2
+                        }
+                        // Puedes agregar más reglas personalizadas aquí
+                    };
+                });
+        }
+        public static void AddApplicationServices(this IServiceCollection services)
+            {
+                // services.AddScoped<IUnitOfWork,UnitOfWork>();
+            }
+    }
+}
